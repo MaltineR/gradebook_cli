e2f234c1619afbd54404f1d1c186682aa4d9b497
MaltineR <maltina.rama@gmail.com>
2026-04-01 20:57:23 +0200

    Add Student and Course functions

--- gradebook/service.py
+++ gradebook/service.py
@@ -9,3 +9,15 @@ class GradebookService:
 
     def save(self):
         save_data(self.data)
+
+    def add_student(self, name):
+        """Add a new student"""
+        new_id = len(self.data["students"]) + 1
+        self.data["students"][str(new_id)] = {"name": name}
+        self.save()
+        return new_id
+
+    def add_course(self, code, title):
+        """Add a new course by code and title"""
+        self.data["courses"][code] = {"title": title}
+        self.save()
