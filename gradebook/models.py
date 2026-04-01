2cc10e218dbdb7f2a79a389a0b5f1d7c43ec9636
MaltineR <maltina.rama@gmail.com>
2026-04-01 17:18:27 +0200

    Enrollment Model

--- gradebook/models.py
+++ gradebook/models.py
@@ -22,3 +22,23 @@ class course :
     def __str__ (self):
         return f"Course (code={self.code}, title={self.title})"
 
+
+""" This is an Enrollment class
+    - Returns a string representation of the enrollment 
+"""
+class enrollment :
+    def __init__(self, student_id, course_code, grades=None):
+        """
+        ID of the student  
+        Code of the Course 
+        List of Grades 
+        """
+        if grades is not None:
+            for g in grades :
+                if g < 0 or g> 100 :
+                    raise ValueError("Grades must be numeric and betwwen 0 and 100")
+        self.student_id=student_id
+        self.course_code=course_code
+        self.grades= grades if grades is not None else []
+    def __str__(self):
+        return f"Enrollment(student={self.student_id},course_code={self.course_code}, grades={self.grades})"
