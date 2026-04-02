e7628fa079a9dd9adf1be59660681987eefc65a4
MaltineR <maltina.rama@gmail.com>
2026-04-02 20:16:02 +0200

    Add a simple logging mechanism

--- gradebook/storage.py
+++ gradebook/storage.py
@@ -1,18 +1,29 @@
 import json
+import logging
 
-F_PATH = "data/gradebook.json"
+F_PATH = "fake/gradebook.json"
 
+
+logging.basicConfig(
+    filename="logs/app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 def load_data():
     """Loads data from the JSON file. Returns empty data if file not found or invalid"""
     try:
         with open(F_PATH, "r") as file:
-            return json.load(file)
+            data = json.load(file)
+            logging.info("Data loaded successfully")
+            return data
 
     except FileNotFoundError:
+        logging.error("File not found")
         print("File not found")
 
     except json.JSONDecodeError:
+        logging.error("Error:Json file is corrupted or empty")
         print("Error: JSON file is corrupted or empty")
 
     return {
@@ -21,7 +32,12 @@ def load_data():
         "enrollments": []
     }
 def save_data(data):
-    """Saves all data to the JSON file"""
-    with open(F_PATH, "w") as file:
-        json.dump(data, file, indent=2)
+    try:
+        """Saves all data to the JSON file"""
+        with open(F_PATH, "w") as file:
+            json.dump(data, file, indent=2)
+            logging.info("Data saved successfully")
+    except Exception as e:
+        logging.error(f"Error saving data: {e}")
+        print("Error saving data")
     
