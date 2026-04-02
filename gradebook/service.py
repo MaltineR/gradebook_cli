3b081332c0471653884bfaef1eef35b65818baf4
MaltineR <maltina.rama@gmail.com>
2026-04-02 15:44:12 +0200

    Replace class with pure functions

--- gradebook/service.py
+++ gradebook/service.py
@@ -1,70 +1,65 @@
 from gradebook.storage import load_data, save_data
 
 
-class GradebookService:
-    """This class handles all operations for the grades system"""
+def add_student(data, name):
+    """Add a new student and return new id"""
+    new_id = len(data["students"]) + 1
+    data["students"][str(new_id)] = {"name": name}
+    return data,new_id
 
-    def __init__(self):
-        self.data = load_data()
 
-    def save(self):
-        save_data(self.data)
+def add_course(data, code, title):
+    """Add a new course"""
+    data["courses"][code] = {"title": title}
+    return data
 
-    def add_student(self, name):
-        """Add a new student"""
-        new_id = len(self.data["students"]) + 1
-        self.data["students"][str(new_id)] = {"name": name}
-        self.save()
-        return new_id
 
-    def add_course(self, code, title):
-        """Add a new course by code and title"""
-        self.data["courses"][code] = {"title": title}
-        self.save()
+def enroll(data, student_id, course_code):
+    """ Enroll a student in a course """
+    data["enrollments"].append({
+        "student_id": student_id,
+        "course_code": course_code,
+        "grades": []
+    })
+    return data
 
-    def enroll_student(self, student_id, course_code):
-        """Enroll a student in a course"""
-        self.data["enrollments"].append({
-            "student_id": student_id,
-            "course_code": course_code,
-            "grades": []
-        })
-        self.save()
 
-    def add_grade(self, student_id, course_code, grade):
-        """Add a grade to a student by checking student id and course code"""
-        for enrollment in self.data["enrollments"]:
-            if enrollment ["student_id"] == student_id and enrollment["course_code"] == course_code:
-                enrollment["grades"].append(grade)
-                self.save()
+def add_grade(data, student_id, course_code, grade):
+    """Add a grade to a student's course"""
+    for e in data["enrollments"]:
+        if e["student_id"] == student_id and e["course_code"] == course_code:
+            e["grades"].append(grade)
 
-    def list_students(self):
-        """List all students by name"""
-        return sorted(self.data["students"].items(), key=lambda x: x[1]["name"])
 
-    def list_courses(self):
-        """List all courses by title"""
-        return sorted(self.data["courses"].items(), key=lambda x: x[1]["title"])
+def list_students(data):
+    """Return students sorted by name"""
+    return sorted(data["students"].items(), key=lambda x: x[1]["name"])
 
-    def list_enrollments(self):
-        """List all enrollments by course code"""
-        return sorted(self.data["enrollments"], key=lambda x: x["course_code"])
-    
-    def compute_average(self, student_id, course_code):
-        """Calculate average grade for a student in a course"""
-        for enrollment in self.data["enrollments"]:
-            if enrollment["student_id"] == student_id and enrollment["course_code"] == course_code:
-                grades = enrollment["grades"]
-                if not grades:
-                    return 0
-                return sum(grades) / len(grades)
 
-    def compute_gpa(self, student_id):
-        """Calculate GPA for a student"""
-        averages = []
-        for enrollment in self.data["enrollments"]:
-            if enrollment["student_id"] == student_id:
-                averages.append(self.compute_average(student_id, enrollment["course_code"]))
-        if not averages:
-            return 0
-        return sum(averages) / len(averages)
+def list_courses(data):
+    """Return courses sorted by title"""
+    return sorted(data["courses"].items(), key=lambda x: x[1]["title"])
+
+
+def list_enrollments(data):
+    """Return enrollments sorted by course code."""
+    return sorted(data["enrollments"], key=lambda x: x["course_code"])
+
+
+def compute_average(data, student_id, course_code):
+    """Calculate average grade for a student in a course"""
+    for e in data["enrollments"]:
+        if e["student_id"] == student_id and e["course_code"] == course_code:
+            grades = e["grades"]
+            return sum(grades) / len(grades) if grades else 0
+    return 0
+
+
+def compute_gpa(data, student_id):
+    """Calculate GPA across all courses"""
+    averages = [
+        compute_average(data, student_id, e["course_code"])
+        for e in data["enrollments"]
+        if e["student_id"] == student_id
+    ]
+    return sum(averages) / len(averages) if averages else 0
